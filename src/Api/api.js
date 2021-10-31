e0fa59956bc78346e298038853daea0db5ad4b7c
Specialboo1 <84511434+Specialboo1@users.noreply.github.com>
2021-10-31 23:39:07 +0530

    server url added

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = `http://localhost:8000`
+const url = `https://boopalanecommerceserver.herokuapp.com`
 
 
 export const authenticateSignup = async (user) => {
